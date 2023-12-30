dab6922b2509c1f7f37294009ba653c4e7018315
MSK1206 <phantom7216@yahoo.co.jp>
2023-12-30 15:18:40 +0900

    Fix: prettier追加による軽微な変更

--- next-app/src/app/layout.tsx
+++ next-app/src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang='ja'>
+    <html lang="ja">
       <body className={inter.className}>{children}</body>
     </html>
   );
--- next-app/src/app/page.tsx
+++ next-app/src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-center p-24'>
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <h1>Hello Docker Next.js 14 App Router.</h1>
     </main>
   );
